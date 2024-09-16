ead9da6677584a3607fcc8af4145e2676f2a9fe6
Baron Yahn <yahnbaron@me.com>
2024-09-15 20:57:41 -0700

    Added compose modal file

---
+++ app/compose.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Modal() {
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
